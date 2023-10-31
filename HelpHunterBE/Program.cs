720668318ef433472579ea0560909d121c8f545a
Konrad Matusiak <matusiak7konrad@gmail.com>
2023-10-31 18:55:58 +0100

    Update Program.cs
    Register password validator service

--- HelpHunterBE/Program.cs
+++ HelpHunterBE/Program.cs
@@ -1,3 +1,6 @@
+using Easy_Password_Validator.Models;
+using Easy_Password_Validator;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json");
 
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient(service => new PasswordValidatorService(new PasswordRequirements()));
 
 var app = builder.Build();
 
