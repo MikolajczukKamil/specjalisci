a9b822046b3e11afd508d056a9fde18c1d268c50
Dragon44PL <krystian.pieniak44@gmail.com>
2023-11-10 12:41:26 +0100

    added map functionality

--- helpHunter-ui/src/app/app.module.ts
+++ helpHunter-ui/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapLocalisationComponent } from './map/map-localisation/map-localisation.component';
+import { NgxMapboxGLModule } from "ngx-mapbox-gl";
+import {GeocodingService} from "./map/map-localisation/geocoding.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
+import {environment} from "../environments/environment.local";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapLocalisationComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NgxMapboxGLModule.withConfig({accessToken: environment.mapToken}),
+        MatCardModule
+    ],
+  providers: [GeocodingService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
