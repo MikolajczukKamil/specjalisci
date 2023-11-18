09225396bbed9baac394febd142fd7ecb5f5f8ea
Maciej Dominiak <maciek1651g@gmail.com>
2023-11-18 11:13:38 +0100

    Unification of components

--- helpHunter-ui/src/app/app-routing.module.ts
+++ helpHunter-ui/src/app/app-routing.module.ts
@@ -3,22 +3,16 @@ import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { LoginMobileComponent } from './login-mobile/login-mobile.component';
-import { HomeMobileComponent } from './home-mobile/home-mobile.component';
-import { RegisterMobileComponent } from './register-mobile/register-mobile.component';
 
 const routes: Routes = [
-    {path: '', component:  window.screen.width > 767 ? HomeComponent : HomeMobileComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'home', component: HomeComponent},
-    {path: 'login-mobile', component: LoginMobileComponent},
-    {path: 'register-mobile', component: RegisterMobileComponent}
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
 ];
-
 
 @NgModule({
     imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
