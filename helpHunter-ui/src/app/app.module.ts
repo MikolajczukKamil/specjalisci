524e7ce83d307b9d3ca49c8608b4a7629769ecb6
Maciej Dominiak <maciek1651g@gmail.com>
2023-11-18 11:48:33 +0100

    Added authorization

--- helpHunter-ui/src/app/app.module.ts
+++ helpHunter-ui/src/app/app.module.ts
@@ -6,11 +6,25 @@ import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { GeneralInterceptor } from './interceptors/general/general.interceptor';
+import { RouterOutlet } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
     declarations: [AppComponent, RegisterComponent, HomeComponent, LoginComponent],
-    imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-    providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterOutlet,
+        FormsModule,
+        MatSnackBarModule,
+    ],
+    providers: [{ provide: HTTP_INTERCEPTORS, useClass: GeneralInterceptor, multi: true }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ helpHunter-ui/src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,7 @@
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+    return inject(AuthService).canActivate();
+};
