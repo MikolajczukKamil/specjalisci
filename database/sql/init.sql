93283e600b46ec26f930ebcfe834da99ff1f9c92
pawelrogulski <pawelrogulski@o2.pl>
2023-12-27 11:31:02 +0100

    Set email as unique in db

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     full_name VARCHAR(100) NOT NULL,
     date_of_birth DATE,
