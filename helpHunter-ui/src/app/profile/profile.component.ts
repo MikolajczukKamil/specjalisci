6ae35599a72386149156d7f06f8b04e10c37b477
Aleksy Pazura <aleksy.pazura@onet.pl>
2024-01-04 17:35:02 +0100

    issue-36 Stworzenie peofilu użytkownika

--- helpHunter-ui/src/app/profile/profile.component.ts
+++ helpHunter-ui/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {ButtonComponent} from "../components/button/button.component";
+import {UserService} from "../services/user/user.service";
+import {UserData} from "./user-data";
 
 @Component({
   selector: 'app-profile',
@@ -6,45 +9,43 @@ import {Component} from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  protected list =
-    [
-      {
-        title: 'Dane kontaktowe',
-        subtitle: 'Zaktualizuj swoje zdjęcie i data osobwe.',
+
+  user?: UserData;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.userService.getUserData().subscribe(
+      (data) => {
+        this.user = data;
       },
-      {
-        title: 'Imię i nazwisko',
-        data: 'Janusz Kowalski'
-      },
-      {
-        title: 'Telefon',
-        data: '123 456 789'
-      },
-      {
-        title: 'Email',
-        data: 'pCQpK@example.com'
-      },
-      {
-        title: 'Twój avatar',
-        subtitle: 'Będzie to widoczne na Twoim profilu',
-        data: './assets/images/avatars/avatar1.png'
-      },
-      {
-        title: 'Adres',
-        data: 'ul. Przyjazna 1, 00-000 Wrocław'
-      },
-      {
-        title: 'Tryb specjalisty'
-      },
-      {
-        title: 'O firmie',
-        data: 'Opis firmy'
-      },
-      {
-        title: 'Tryb pracy'
-      },
-      {
-        title: 'Lista usług'
-      },
-    ]
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  isEdit = false;
+  isOn = false;
+
+  public editData() {
+    this.isEdit = true;
+  }
+
+  public cancel() {
+    this.isEdit = false;
+  }
+
+  public save() {
+    this.isEdit = false;
+  }
+
+  public swipeOnOff() {
+    this.isOn = !this.isOn;
+  }
+
 }
