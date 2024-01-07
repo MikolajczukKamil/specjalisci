449385be03943d3e1ee31b0ca034779ebc448391
Krzysztof Wójcik <krzysztofw.2744@gmail.com>
2024-01-07 14:58:33 +0100

    Fix nullable fields, minor improvements

--- HelpHunterBE/Logic/UserLogic.cs
+++ HelpHunterBE/Logic/UserLogic.cs
@@ -1,4 +1,5 @@
 ﻿using HelpHunterBE.Dto;
+using HelpHunterBE.utils;
 using Npgsql;
 using System.Net;
 
@@ -34,16 +35,15 @@ namespace HelpHunterBE.Logic
                             user.Id = reader.GetInt32(0);
                             user.Username = reader.GetString(1);
                             user.Email = reader.GetString(2);
-                            user.Password = reader.GetString(3);
                             user.Fullname = reader.GetString(4);
                             user.Birthdate = reader.GetDateTime(5);
                             user.IsProvidingServices = reader.GetBoolean(6);
                             user.Location = reader.GetString(7);
                             user.Latitude = reader.GetDecimal(8);
                             user.Longitude = reader.GetDecimal(9);
-                            user.Avatar = reader.GetInt32(10);
-                            user.PhoneNumber = reader.GetString(11);
-                            user.Description = reader.GetString(12);
+                            user.Avatar = reader.GetNullableField<int>(10);
+                            user.PhoneNumber = reader.GetNullableField<string>(11);
+                            user.Description = reader.GetNullableField<string>(12);
                         }
                     }
                 }
@@ -60,23 +60,22 @@ namespace HelpHunterBE.Logic
                 {
                     connection.Open();
 
-                    string query = "UPDATE users SET username = @Username, email = @Email, password = @Password, full_name = @Fullname, date_of_birth = @Birthdate, is_providing_services = @IsProvidingServices, location = @Location, location_coordinates_x = @Latitude, location_coordinates_y = @Longitude, avatar = @Avatar, phone_number = @PhoneNumber, description = @Description WHERE user_id = @Id";
+                    string query = "UPDATE users SET username = @Username, email = @Email, full_name = @Fullname, date_of_birth = @Birthdate, is_providing_services = @IsProvidingServices, location = @Location, location_coordinates_x = @Latitude, location_coordinates_y = @Longitude, avatar = @Avatar, phone_number = @PhoneNumber, description = @Description WHERE user_id = @Id";
 
                     using (var command = new NpgsqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@Id", userDto.Id);
                         command.Parameters.AddWithValue("@Username", userDto.Username);
                         command.Parameters.AddWithValue("@Email", userDto.Email);
-                        command.Parameters.AddWithValue("@Password", userDto.Password);
                         command.Parameters.AddWithValue("@Fullname", userDto.Fullname);
                         command.Parameters.AddWithValue("@Birthdate", userDto.Birthdate);
                         command.Parameters.AddWithValue("@IsProvidingServices", userDto.IsProvidingServices);
                         command.Parameters.AddWithValue("@Location", userDto.Location);
                         command.Parameters.AddWithValue("@Latitude", userDto.Latitude);
                         command.Parameters.AddWithValue("@Longitude", userDto.Longitude);
-                        command.Parameters.AddWithValue("@Avatar", userDto.Avatar);
-                        command.Parameters.AddWithValue("@PhoneNumber", userDto.PhoneNumber);
-                        command.Parameters.AddWithValue("@Description", userDto.Description);
+                        command.Parameters.AddWithValue("@Avatar", userDto.Avatar ?? 0);
+                        command.Parameters.AddWithValue("@PhoneNumber", userDto.PhoneNumber ?? "");
+                        command.Parameters.AddWithValue("@Description", userDto.Description ?? "");
 
                         command.ExecuteNonQuery();
                     }
@@ -84,8 +83,9 @@ namespace HelpHunterBE.Logic
 
                 return HttpStatusCode.OK;
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
+                Console.WriteLine(exception.Message);
                 return HttpStatusCode.InternalServerError;
             }
         }
