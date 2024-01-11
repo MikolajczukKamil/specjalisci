a01c5449ae4173e55653fa8689e74e698cf83fae
Maciej Dominiak <maciek1651g@gmail.com>
2024-01-11 22:56:23 +0100

    Fix

--- helpHunter-ui/src/app/interceptors/general/general.interceptor.ts
+++ helpHunter-ui/src/app/interceptors/general/general.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, isDevMode } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth/auth.service';
@@ -6,14 +6,13 @@ import { AuthService } from '../../services/auth/auth.service';
 @Injectable()
 export class GeneralInterceptor implements HttpInterceptor {
     private readonly apiServer = '//95.217.193.230:9999';
-    private readonly isDevMode = isDevMode();
 
     private allowedUrls = ['/api/login', '/api/register', 'api.mapbox.com'];
 
     constructor(private auth: AuthService) {}
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        if (!isDevMode && request.url.startsWith('/')) {
+        if (request.url.startsWith('/')) {
             request = request.clone({
                 url: this.apiServer + request.url,
             });
